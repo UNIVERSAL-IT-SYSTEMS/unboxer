0e0e783bbd0f62285ccf37f0c28a487b55b5de05
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2016-02-01 22:49:04 -0800

    some more code

--- bin/unboxer.js
+++ bin/unboxer.js
@@ -4,33 +4,55 @@
 var fs = require('fs'),
     path =require('path'),
     yargs = require('yargs'),
+    browserify = require('browserify'),
+    jsof = require('jsof'),
     onml = require('onml');
 
 var argv = yargs.argv;
 
 function assets (ml) {
-    var res = [];
+    var name, d, type, res = {};
     onml.traverse(ml, {
         enter: function (node) {
             if (node.attr.id && node.attr.id.match(/mv\-/)) {
-                res.push(node.full);
-                this.skip();
+                name = node.attr.id.slice(3);
+                d = node.attr.d.split(' ');
+                type = d.shift();
+                d = d.map(function (e) {
+                    return e.split(',').map(function (e) {
+                        return Number(e);
+                    });
+                });
+                if (type === 'm') {
+                    res[name] = { dx: d[1][0], dy: d[1][1] };
+                } else {
+                    throw 'unknown path type: ' + node.attr.d;
+                }
+                this.remove();
             }
         }
     });
-    return res;
+    return jsof.s(res);
 }
 
 function f2o (name, cb) {
-    var full, ml;
+    var full, ml, res;
     full = path.resolve(process.cwd(), name);
     fs.readFile(full, { encoding: 'utf8'}, function (err, dat) {
         if (err) {
             throw err;
         }
         ml = onml.parse(dat);
-        ml = assets(ml)
-        cb(ml);
+        res = `
+'use strict';
+var unboxer = require('../lib');
+var assets = {
+moves: ${assets(ml)},
+template: ${jsof.s(ml)}
+};
+unboxer(assets);
+`;
+        cb(res);
     });
 }
 
